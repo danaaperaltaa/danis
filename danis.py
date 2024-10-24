cad0cabea828e5e1218ea27b744b22dfaea97fbf
danaaperaltaa <dana.peralta1911@gmail.com>
2024-10-24 20:45:54 +0000

    d

---
+++ danis.py
@@ -0,0 +1,13 @@
+empieza=1
+termina=11
+for contador in range(empieza,termina):
+    print(contador)
+    boleano=True or False
+    boleano=True and False
+    boleano=not True
+    boleaano=18<20
+    boleano=1==1
+    boleano=empieza==1
+    boleano=20>=16
+    boleano=8<=8
+    boleano=(1==1)
