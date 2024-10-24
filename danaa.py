58f8090a4974555181668d5a6d247ea147ab1e0d
danaaperaltaa <dana.peralta1911@gmail.com>
2024-10-24 21:01:02 +0000

    d

---
+++ danaa.py
@@ -0,0 +1,10 @@
+nombre="Dana"
+print("ingrese su nombre")
+nombre=inpot()
+print("hola" +nombre)
+string=nombre
+boleano=True or False
+while contador1>20:
+    for contador2 in range(20):
+        if (contador1%2=0) and (contador2%2=1):
+        print(contador1+  +contador2)
